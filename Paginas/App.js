0dd85579fa5d23811752641423c9e2f08ccd30dc
Dandrea Omar <omar_dandrea97@hotmail.com>
2020-09-09 01:25:59 -0300

    App.js arreglado, quedan las pistas por agregar

--- Paginas/App.js
+++ Paginas/App.js
@@ -2,7 +2,7 @@
 //     let preguntas1 = ["¿Nombre del vocalista principal?","Participó de la banda Dead By Sunrise ","Nombre del Primer album","Grabo 7 álbumes","fue un músico, cantante, compositor y actor estadounidense"];
 
 //     let preguntas2 = ["Cual fue el álbum debut de la banda","Fue publicado por Warner Bros. Records en el 2000","Fue un éxito comercial tras vender, a la fecha 30 millones de copias a nivel mundial.","El album tiene un nombre parecido a  "];
-    
+
 // }
 
 // const respuesta = 
@@ -39,32 +39,63 @@
 //       alert('[ERROR] El campo debe tener un valor de...');
 //       return false;
 //     }
-    
+
 //     else if ()  {//condicion que debe cumplir el último campo del formulario) {
 //       // Si no se cumple la condicion...
 //       alert('[ERROR] El campo debe tener un valor de...');
 //       return false;
 //     }
-  
+
 //     // Si el script ha llegado a este punto, todas las condiciones
 //     // se han cumplido, por lo que se devuelve el valor true
 //     return true;
 //   }
 
-var resp = document.getElementById('respuesta')
-var error = document.getElementById('error')
-function adivinanza(){
-    
-    if(resp.value == "hola"){
-        alert('acertaste...');
+
+let resp = document.getElementById('respuesta');
+let error = document.getElementById('error');
+let intentos = document.getElementById('numintentos');
+let contador = intentos;
+function adivinanza() {
+
+    if (resp == 'hola') {
+
+        alert('¡¡¡¡EXCELENTE acertaste a la adivinanza..!!!');
         return false;
+    } else if (resp != 'hola') {      // && intentos ==4) {
 
-    }else{
-        alert('[ERROR] El campo debe tener un valor de...');
-    return false;
+        if (contador.value == '4') {
+            contador.value--;
+            document.getElementById('numintentos').value = contador;
+            alert('[ERROR] El campo debe tener un valor de...');
+            //AGREGAR PISTA
+
+        } else if (contador.value == 3) {
+            contador.value--;
+            document.getElementById('numintentos').value = contador;
+            alert('[ERROR] El campo debe tener un valor de...');
+            //AGREGAR PISTA
+        }else if (contador.value == 2) {
+            contador.value--;
+            document.getElementById('numintentos').value = contador;
+            alert('[ERROR] El campo debe tener un valor de...');
+            //AGREGAR PISTA
+            
+        } else {
+            contador--;
+            document.getElementById('numintentos').value = contador;
+            alert('PERDISTE');
+            //AGREGAR PISTA
+            
+        }
+
     }
+    alert('[ERROR] sss...');
 
 
 
-    
+
+
+
+
 }
